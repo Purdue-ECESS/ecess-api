01ef10d56a6e6c5e87dca3533fc857a2cbd2688a
Matthew Wen <mattwen2018@gmail.com>
2021-10-20 01:58:39 -0400

    setting up

---
+++ index.ts
@@ -0,0 +1,61 @@
+import express from "express";
+import expressWs from "express-ws";
+
+const wsExpress = expressWs(express());
+const app = wsExpress.app;
+const port = process.env.PORT || 3000;
+
+app.use((req: any, res: any, next: any) => {
+    console.log("middleware");
+    return next();
+});
+
+app.get("/", (req: any, res: any) => {
+    res.send({
+        status: 'development',
+        purpose: 'ECE Ambassadors',
+        owner: 'Purdue ECE'
+    });
+});
+
+
+app.ws('/ws', (ws_param: any, req: any) => {
+    ws_param.on("message", (msg: string) => {
+        console.log(msg);
+    })
+});
+
+app.ws('/hello/:world', function(ws, req, next) {
+
+    let interval: NodeJS.Timer | undefined = undefined;
+    let message = "sending data back";
+
+    ws.on('open', function () {
+        console.log('open connection');
+    })
+    ws.on('message', function(msg: string, b) {
+        console.log(msg);
+        if (msg === "stream" && interval === undefined) {
+            interval = setInterval(() => {
+                ws.send(message);
+            }, 1000);
+        }
+        else if (msg === "end" && interval) {
+            clearInterval(interval);
+            interval = undefined;
+        }
+        else {
+            message = msg;
+        }
+    });
+    ws.on("close", function() {
+        if (interval) {
+            console.log("close interval");
+            clearInterval(interval);
+            interval = undefined;
+        }
+    });
+    next();
+});
+
+app.listen(port);
