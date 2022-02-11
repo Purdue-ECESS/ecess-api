a8c6e2e15523cd781e67f6b2573d1c14bd9745c9
Matthew Wen <mattwen2018@gmail.com>
2022-02-11 13:36:00 -0500

    Read Events from Firebase

--- src/google/myFb/myFbDb.ts
+++ src/google/myFb/myFbDb.ts
@@ -15,4 +15,33 @@ export class MyFbDb extends MyFirebase {
         const response = await this.default.firestone.collection("testing").doc("ibf39bPxk3il3uQtzacL").get();
         console.log(response);
     }
+
+    static async addDiscordEvents(events: any[]) {
+        const promises: any[] = [];
+        events.forEach((item) => {
+            promises.push(this.default.firestone.collection("events").doc(item.id).set(item));
+        });
+        await Promise.all(promises);
+    }
+
+    static async getEvents(dateStr: string | undefined) {
+        let date = new Date();
+        if (dateStr) {
+            date = new Date(dateStr);
+        }
+        const query = date.toISOString().split('T')[0] + "T99:9:99:999Z";
+        const response = await this.default.firestone
+            .collection("events")
+            .where("end.dateTime", "<=",  query)
+            .orderBy("end.dateTime", "asc").get();
+        const items: any[] = [];
+        const startWith = date.toISOString().split('T')[0];
+        response.forEach((item) => {
+            const json = item.data();
+            if (dateStr == undefined || json.end.dateTime.startsWith(startWith)) {
+                items.push(item.data());
+            }
+        });
+        return items;
+    }
 }
